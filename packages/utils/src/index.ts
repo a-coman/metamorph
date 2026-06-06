884bfcef25dec2f831aeeef4828e0e12bff18189
a-coman <141495654+a-coman@users.noreply.github.com>
2026-06-06 17:56:12 +0200

    Build @metamorph/utils dist exports for workspace consumption.

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,20 +1,13 @@
 export {
   AggregateRoot,
-  DomainEvent,
   DomainEvents,
   Entity,
   UniqueEntityID,
   ValueObject,
 } from './ddd.js';
+export type { DomainEvent } from './ddd.js';
 
-export {
-  DomainError,
-  Either,
-  Left,
-  Result,
-  Right,
-  left,
-  right,
-} from './errors.js';
+export { Left, Result, Right, left, right } from './errors.js';
+export type { DomainError, Either } from './errors.js';
 
 export { Repository } from './repository.js';
